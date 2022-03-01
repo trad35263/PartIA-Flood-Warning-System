05278e395a187857e209423dded39e952f9904a7
rard2@cam.ac.uk <rard2@cam.ac.uk>
2022-03-01 02:50:41 +0000

    predict future levels function fix

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -17,3 +17,18 @@ def polyfit(dates, levels, p):
     poly = np.poly1d(p_coeff)
 
     return poly, d0
+
+# for Task 2G
+
+def predict_future_level(dates, levels):
+    assert len(dates) == len(levels)
+    assert len(dates) > 0
+
+    x = matplotlib.dates.date2num(dates)
+    d0 = sum(x) / len(x)
+    coeff = np.polyfit(x - d0, levels, 3)
+    poly = np.poly1d(coeff)
+
+    # one day after the most recent day
+    future_date = max(x) + 1
+    return matplotlib.dates.num2date(future_date), poly(future_date - d0)
